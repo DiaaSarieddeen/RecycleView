06f0eca7ca939490d4160df820999cfa7b9db9c5
Diaa <diaa.sarieddine@st.ul.edu.lb>
2022-12-24 10:33:43 +0200

    added the recycler vieww

---
+++ app/src/main/java/com/daniel/recycleview/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package com.daniel.recycleview.model
+
+//we transformed it into data class because it takes the strings found in strings.xml and then populate them
+data class Affirmation(val stringResourceID: Int)  {
+
+}
